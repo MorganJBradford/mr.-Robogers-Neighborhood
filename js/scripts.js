0bacb52b6f1169c5ba3f1d461aea54f041705ea5
Morgan Bradford <morganjbradford95@gmail.com>
2021-01-22 11:33:46 -0600

    add return to branching statements; add cons beepityBoppityBoop

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,16 @@ let beepBoopReturn = [];
 function beepBoop(response) {
   if (response === 0){
     beepBoopReturn.push(response);
+    return beepBoopReturn
   } else if (response === 3) {
     beepBoopReturn = "Won't you be my neighbor"
+    return beepBoopReturn
   } else if (response === 2) {
     beepBoopReturn = "Boop!"
+    return beepBoopReturn
   } else if (response === 1) {
     beepBoopReturn = "Beep!"
+    return beepBoopReturn
   }
 }
 
@@ -18,5 +22,6 @@ $(document).ready(function(){
   $("#formOne").submit(function(event) {
     event.preventDefault()
     response = (parseInt($("#userNumber").val()));
+    const beepityBoppityBoop = beepBoop(response)
   });
 });
