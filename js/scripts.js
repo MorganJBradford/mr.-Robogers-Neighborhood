91ddda81351baa25ae25e157009d62565be8be25
Morgan Bradford <morganjbradford95@gmail.com>
2021-01-26 12:33:52 -0600

    add .includes to branching statement; separate functions, call beepBoopNumReeplacer() within beepBoop()

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,38 @@
 let beepBoopReturn = [];
 
-let response;
-function beepBoop(){
+function beepBoop(response){
   if (parseInt(response) >= 0) {
     for (let i = 0; i <= parseInt(response); i++) {
-      beepBoopReturn.push(i);
-    }
-  }
-  function beepBoopNumReplacer(beepBoopReturn) {
-    for (let i = 0; i < beepBoopReturn.length; i++) {
-      if (beepBoopReturn[i] === 0){
-        beepBoopReturn[i] = (parseInt(beepBoopReturn[i]));
-      } else if (beepBoopReturn[i] === 3) {
-        beepBoopReturn[i] = "Won't you be my neighbor?"
-      } else if (beepBoopReturn[i] === 2) {
-        beepBoopReturn[i] = "Boop!"
-      } else if (beepBoopReturn[i] === 1) {
-        beepBoopReturn[i] = "Beep!"
-      }
+      beepBoopReturn.push(i.toString());
     }
   }
   beepBoopNumReplacer(beepBoopReturn)
+}
+function beepBoopNumReplacer(beepBoopReturn) {
+  for (let i = 0; i < beepBoopReturn.length; i++) {
+    if (beepBoopReturn[i] === "0"){
+      beepBoopReturn[i] = (parseInt(beepBoopReturn[i]));
+    } else if (beepBoopReturn[i].includes("3")) {
+      beepBoopReturn[i] = "Won't you be my neighbor?"
+    } else if (beepBoopReturn[i].includes("2")) {
+      beepBoopReturn[i] = "Boop!"
+    } else if (beepBoopReturn[i].includes("1")) {
+      beepBoopReturn[i] = "Beep!"
+    }
+  }
 }
 
 
 $(document).ready(function(){
   $("#formOne").submit(function(event) {
     event.preventDefault()
-    response = ($("#userNumber").val());
-    const beepityBoppityBoop = beepBoop(response);
-    $("#output").text(beepityBoppityBoop);
+    let response = ($("#userNumber").val());
+    $("#output").text(beepBoop(response));
     $(".output").show();
   });
 });
+
+
+
+
+
